d63f228653ff25488b844786590388a1ddc598bc
Hobson Lane <hobson@totalgood.com>
2013-11-15 15:16:22 -0800

    initial attempt at oo

--- demo-optimized/example/__init__.py
+++ demo-optimized/example/__init__.py
@@ -2,6 +2,37 @@ from datetime import datetime
 from django.db import connection as con
 import sqlparse
 
+class QueryTimer(object):
+    """Based on https://github.com/jfalkner/Efficient-Django-QuerySet-Use"""
+
+    def __init__(self, time=None, num_queries=None, sql=''):
+        self.time, self.num_queries = time, num_queries
+        self.start_time, self.start_queries = None, None
+        self.sql = sql
+        self.start()
+
+    def start(self):
+        self.queries = []
+        self.start_time = datetime.now()
+        self.start_queries = len(con.queries)
+
+    def stop(self):
+        self.time = datetime.now() - self.start_time
+        self.queries = con.queries[self.start_queries:]
+        self.num_queries = len(self.queries)
+
+    def format_sql(self):
+        if self.time is None or self.queries is None:
+            self.stop()
+        if self.queries or not self.sql:
+            self.sql = []
+            for query in self.queries:
+                self.sql += [sqlparse.format(query['sql'], reindent=True, keyword_case='upper')]
+        return self.sql
+
+    def __repr__(self):
+        return '%s(time=%s, num_queries=%s, sql=%s)' % (self.__class__.__name__, self.time, self.num_queries)
+
 
 def start():
     global start_time
